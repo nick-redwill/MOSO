69ca78cbd61d4d17de4c997f7b101ab41d73f535
redwill <hro.2016@yandex.ru>
2026-04-03 19:00:57 +0400

    add support for gparted and system rescue ISOs

---
+++ scripts/linux/gparted.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# scripts/linux/gparted.sh
+
+source "$(dirname "${BASH_SOURCE[0]}")/linux.sh"
+
+SYSTEM_NAME="GParted"
---
+++ scripts/linux/sysrescue.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# scripts/linux/gparted.sh
+
+source "$(dirname "${BASH_SOURCE[0]}")/linux.sh"
+
+SYSTEM_NAME="System Rescue"
+
+grub_entry() {
+    local uuid=$1
+    local part=$2
+    cat << EOF
+
+menuentry "$SYSTEM_NAME ($part)" {
+    search --no-floppy --set=root --fs-uuid $uuid
+    configfile /boot/grub/grubsrcd.cfg
+}
+EOF
+}
